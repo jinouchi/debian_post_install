42bd8a4db51c03f6eb8e6de86b859ae683367efe
jinouchi <my.unique.screen.name@gmail.com>
2019-09-16 13:15:17 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,11 @@
 
 DISTRO="stretch"
 
-echo "# Begin user added repos: 
+# Comment out existing sources
+sed --in-place=".bak" 's/^deb/# deb/g' /etc/apt/sources.list
+
+echo "
+# Begin user added repos: 
 deb http://deb.debian.org/debian $DISTRO main contrib non-free
 deb-src http://deb.debian.org/debian $DISTRO main contrib non-free
 
