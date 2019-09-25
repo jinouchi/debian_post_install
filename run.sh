291cbd011f3dc8d9657c16e5532f52c72ef2cfc8
Jinouchi <my.unique.screen.name@gmail.com>
2019-09-25 11:51:46 -0600

    modularization

--- run.sh
+++ run.sh
@@ -1,23 +1,8 @@
-# Add repos to /etc/apt/sources.list and update/upgrade (change DISTRO to correct distro if needed): 
+#!/bin/bash
 
-DISTRO="stretch"
-
-# Comment out existing sources
-sed --in-place=".bak" 's/^deb/# deb/g' /etc/apt/sources.list
-
-echo "
-# Begin post-install script added repos: 
-deb http://deb.debian.org/debian $DISTRO main contrib non-free
-deb-src http://deb.debian.org/debian $DISTRO main contrib non-free
-
-deb http://deb.debian.org/debian-security/ $DISTRO/updates main contrib non-free
-deb-src http://deb.debian.org/debian-security/ $DISTRO/updates main contrib non-free
-
-deb http://deb.debian.org/debian $DISTRO-updates main contrib non-free
-deb-src http://deb.debian.org/debian $DISTRO-updates main contrib non-free" >> /etc/apt/sources.list
-
-apt update
-apt upgrade -y
+./update_apt_sources.sh
+ 
+./upgrade.sh
 
 # Install sudo:
 apt install sudo git -y
---
+++ update_apt_sources.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Add repos to /etc/apt/sources.list and update/upgrade (change DISTRO to correct distro if needed): 
+
+DISTRO="stretch"
+
+# Comment out existing sources
+sed --in-place=".bak" 's/^deb/# deb/g' /etc/apt/sources.list
+
+echo "
+# Begin post-install script added repos: 
+deb http://deb.debian.org/debian $DISTRO main contrib non-free
+deb-src http://deb.debian.org/debian $DISTRO main contrib non-free
+
+deb http://deb.debian.org/debian-security/ $DISTRO/updates main contrib non-free
+deb-src http://deb.debian.org/debian-security/ $DISTRO/updates main contrib non-free
+
+deb http://deb.debian.org/debian $DISTRO-updates main contrib non-free
+deb-src http://deb.debian.org/debian $DISTRO-updates main contrib non-free" >> /etc/apt/sources.list
